1ba8938f0a9bae64e61bacbe1a0a6f9b4c419602
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-02-29 16:36:43 +0600

    songs fetched done from the songs directory

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 console.log('javascript started!');
 
 
+async function fetchSongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+    console.log(response);
 
-let a = fetch("http://127.0.0.1:5500/songs/")
+    let div = document.createElement('div');
+    div.innerHTML = response;
+    // let lis = div.getElementsByTagName('li');
+    // console.log(lis);
+
+
+    let as = div.getElementsByTagName('a');
+    // console.log(as);
+
+    //to get all the songs from the page and store them in an array
+
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith('.mp3')) {
+            songs.push(element.href);
+        }
+    }
+
+    // console.log(songs);
+    return songs;
+
+}
+
+fetchSongs();
